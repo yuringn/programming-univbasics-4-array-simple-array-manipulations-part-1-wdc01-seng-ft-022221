a7c338c020aebcd85ca5787d5225bb3f9720b4a5
Yuri Ng <yuringuyen312@gmail.com>
2021-02-06 06:13:10 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,3 +2,20 @@ def using_push(array, string)
   array.push(string)
 end
   using_push([1,2,3,4], "a")
+
+def using_unshift(array,string)
+array.unshift(string)
+end
+using_unshift(["a","b","c","d"],"aa")
+
+def using_pop(array)
+  array.pop
+end
+using_pop([1,2,3,4])
+
+def pop_with_args(array)
+  array.pop(2)
+end
+  pop_with_args([5,6,7,8])
+
+  
