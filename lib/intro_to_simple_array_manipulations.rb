10dd965cc694ee2676abfd4ef5352f927f24cec0
Yuri Ng <yuringuyen312@gmail.com>
2021-02-06 06:17:20 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -18,4 +18,12 @@ def pop_with_args(array)
 end
   pop_with_args([5,6,7,8])
 
-  
+  def using_shift(array)
+    array.shift
+  end
+  using_shift(["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone"])
+
+  def shift_with_args(array)
+    array.shift(2)
+  end
+  shift_with_args(["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"])
